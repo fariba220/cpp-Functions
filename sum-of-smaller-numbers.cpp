e63c71eb28273890714444beaebdf6cdb14dd35b
faribadev <95432972+fariba220@users.noreply.github.com>
2026-06-28 01:40:15 +0330

    sum-of-smaller-numbers
    # Sum of Numbers Less Than N (C++)
    
    This program gets a number from the user and calculates the sum of all numbers smaller than that number.
    
    ## Files
    
    * `main.cpp`
    * `sum.cpp`
    * `sum.h`
    
    ## Example
    
    Input:
    5
    
    Output:
    10
    
    Because:
    1 + 2 + 3 + 4 = 10
    
    ## Language
    
    C++

---
+++ sum-of-smaller-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "sum of min header.h"
+using namespace std;
+
+    int main()
+ {
+    int n;
+    cout<<"enter number:";
+    cin>>n;
+
+    cout<<"sum is ="<<jam(n) ;
+    return 0;
+}
